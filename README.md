05f38f8eee2391f59f9372162cf115cf3c7534d9
Bhadresh Desai <16066580+bhadreshdesai@users.noreply.github.com>
2024-05-19 15:16:18 +0000

    add axum server

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 <!-- TOC -->
 
 - [rust-app](#rust-app)
-    - [instructions](#instructions)
-        - [step 1](#step-1)
+  - [instructions](#instructions)
+    - [step 1 - setup tokio](#step-1---setup-tokio)
+    - [step 2 - setup axum](#step-2---setup-axum)
 
 <!-- /TOC -->
 
 # rust-app
+
 Demo rust application
 
 ## instructions
-### step 1
+
+### step 1 - setup tokio
+
 ```shell
 # initialise a new app
 caro init
@@ -18,5 +22,20 @@ caro init
 cargo add tokio -F full
 # update main.rs
 # run app, expect Hello, world! printed in the console
-cargo run 
+cargo run
+```
+
+### step 2 - setup axum
+
+```shell
+cargo add axum
+cargo add --dev tower -F util
+cargo add --dev http-body-util
+# add the routes function in main.rs and add the tests
+# run test, it should pass
+cargo test
+# run the server, the server should start listening on port 3000
+cargo run
+# in a separate terminal use curl, should return Hello, World!!
+curl http://localhost:3000/plaintext
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,43 @@
+use axum::{routing::get, Router};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, routes()).await.unwrap();
+}
+
+fn routes() -> Router {
+    Router::new().route("/plaintext", get(|| async { "Hello, World!!" }))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt; // for `collect`
+    use tower::ServiceExt; // for `oneshot`
+
+    #[tokio::test]
+    async fn plaintext() {
+        let routes = routes();
+        let response = routes
+            .oneshot(
+                Request::builder()
+                    .uri("/plaintext")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        //assert_eq!(&body[..], b"Hello, World!");
+        // convert to string for comparision so if there is a difference it is displayed as string instead of bytes which is difficult to review
+        assert_eq!(String::from_utf8(body.to_vec()).unwrap(), "Hello, World!!");
+    }
 }
