6af1766e5561f61022d1d0232a90dbc601d318a0
Bhadresh Desai <16066580+bhadreshdesai@users.noreply.github.com>
2024-05-19 23:17:23 +0000

    implement todos_get api

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 <!-- TOC -->
 
 - [rust-app](#rust-app)
-  - [instructions](#instructions)
-    - [step 1 - setup tokio](#step-1---setup-tokio)
-    - [step 2 - setup axum](#step-2---setup-axum)
+    - [instructions](#instructions)
+        - [step 1 - setup tokio](#step-1---setup-tokio)
+        - [step 2 - setup axum](#step-2---setup-axum)
+        - [step 3 - implement todos_get api](#step-3---implement-todos_get-api)
 
 <!-- /TOC -->
 
@@ -38,4 +39,22 @@ cargo test
 cargo run
 # in a separate terminal use curl, should return Hello, World!!
 curl http://localhost:3000/plaintext
+```
+
+### step 3 - implement todos_get api
+
+```shell
+cargo add serde -F derive
+cargo add --dev serde_json
+# create ToDo struct and Db using ToDo
+# implement todos_get
+# create dummy todos and add it to db in routes()
+# add get /todos route using todos_get
+# implement test_todos_get  test
+# run test, it should pass 2 tests
+cargo test
+# run the server, the server should start listening on port 3000
+cargo run
+# in a separate terminal use curl, should return 2 ToDos. return content-type: application/json
+curl -vs  http://localhost:3000/todos | jq
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,47 @@
-use axum::{routing::get, Router};
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
+
+use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
+use serde::Serialize;
+
+#[derive(Debug, Serialize, Clone)]
+struct Todo {
+    id: i32,
+    text: String,
+    completed: bool,
+}
+
+type Db = Arc<RwLock<HashMap<i32, Todo>>>;
+
+async fn todos_get(State(db): State<Db>) -> impl IntoResponse {
+    let todos = db.read().unwrap();
+    let todos = todos.values().cloned().collect::<Vec<_>>();
+    return Json(todos);
+}
+
+fn routes() -> Router {
+    let db = Db::default();
+    // create a ToDo
+    let todo = Todo {
+        id: 1,
+        text: "First ToDo".to_owned(),
+        completed: false,
+    };
+    db.write().unwrap().insert(todo.id, todo.clone());
+    let todo = Todo {
+        id: 2,
+        text: "Second ToDo".to_owned(),
+        completed: false,
+    };
+    db.write().unwrap().insert(todo.id, todo.clone());
+
+    return Router::new()
+        .route("/plaintext", get(|| async { "Hello, World!!" }))
+        .route("/todos", get(todos_get))
+        .with_state(db);
+}
 
 #[tokio::main]
 async fn main() {
@@ -6,10 +49,6 @@ async fn main() {
         .await
         .unwrap();
     axum::serve(listener, routes()).await.unwrap();
-}
-
-fn routes() -> Router {
-    Router::new().route("/plaintext", get(|| async { "Hello, World!!" }))
 }
 
 #[cfg(test)]
@@ -20,10 +59,11 @@ mod tests {
         http::{Request, StatusCode},
     };
     use http_body_util::BodyExt; // for `collect`
+    use serde_json::{json, Value};
     use tower::ServiceExt; // for `oneshot`
 
     #[tokio::test]
-    async fn plaintext() {
+    async fn test_plaintext() {
         let routes = routes();
         let response = routes
             .oneshot(
@@ -39,5 +79,26 @@ mod tests {
         //assert_eq!(&body[..], b"Hello, World!");
         // convert to string for comparision so if there is a difference it is displayed as string instead of bytes which is difficult to review
         assert_eq!(String::from_utf8(body.to_vec()).unwrap(), "Hello, World!!");
+    }
+
+    #[tokio::test]
+    async fn test_todos_get() {
+        let routes = routes();
+        let response = routes
+            .oneshot(
+                Request::builder()
+                    .uri("/todos")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(
+            body,
+            json!([{ "id": 1, "text": "First ToDo", "completed": false}, { "id": 2, "text": "Second ToDo", "completed": false}])
+        );
     }
 }
